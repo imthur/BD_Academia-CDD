59451ca5ce8d243e0a0a4c5c9b7647f6d6a1f96f
Arthur Aguiar <101827286+imthur@users.noreply.github.com>
2023-11-19 21:42:36 -0300

    Add files via upload

---
+++ DumpAcademia.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: desafioacademia
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alunos` (
+  `matricula` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `telefone` char(11) NOT NULL,
+  `endereço` varchar(255) NOT NULL,
+  `cpf` char(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alunos`
+--
+
+LOCK TABLES `alunos` WRITE;
+/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
+INSERT INTO `alunos` VALUES (1,'Vicente Elias','12345678910','Rua Joãozinho, 50','12345678910','vicenteelias@teste.com'),(2,'Arthur','81995751832','rua tal tal, 123','12345678999','arthur@teste.com'),(3,'Arthur','45326321631','rua exemplo, 12','12390312345','teste@arthur.com'),(4,'Junior','15161234565','Rua etc e tal, 100','78907890781','junior@teste.com');
+/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionarios`
+--
+
+DROP TABLE IF EXISTS `funcionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcionarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` char(11) NOT NULL,
+  `salario` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionarios`
+--
+
+LOCK TABLES `funcionarios` WRITE;
+/*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
+INSERT INTO `funcionarios` VALUES (1,'Leticia','12332145665',1500.90);
+/*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modalidades`
+--
+
+DROP TABLE IF EXISTS `modalidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modalidades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(510) DEFAULT NULL,
+  `duracao` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modalidades`
+--
+
+LOCK TABLES `modalidades` WRITE;
+/*!40000 ALTER TABLE `modalidades` DISABLE KEYS */;
+INSERT INTO `modalidades` VALUES (2,'Natação','Treino de nado em piscina olímpica, com professores formados.','3h/semana'),(3,'Musculação',' Aparelhos de musculação disponíveis em horário integral','Livre');
+/*!40000 ALTER TABLE `modalidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `cpf` char(11) NOT NULL,
+  `cref` char(6) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `telefone` char(11) NOT NULL,
+  `endereço` varchar(255) NOT NULL,
+  PRIMARY KEY (`cref`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES ('12345678910','123456','José','81999999999','rua exemplo de tal, 123'),('56789012343','123899','Pietro','34567890212','Rua coisa e tal, 900');
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-19 21:38:57
---
+++ crud.py
@@ -0,0 +1,188 @@
+import mysql.connector
+
+def menu():
+    print("=" * 35)
+    print(" MENU DE ADMINISTRAÇÃO DA ACADEMIA")
+    print("=" * 35)
+    print("[1] Inserir")
+    print("[2] Remover")
+    print("[3] Procurar")
+    print("[0] Sair")
+    print("=" * 35)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="ARTHUUR132",
+    database="desafioacademia"
+)
+mycursor = mydb.cursor()
+loop = True
+while loop:
+    menu()
+    choice = input("Digite o dígito da operação que deseja efetuar: ")
+    while True:
+        print("=" * 35)
+        print("Qual será o tipo de dado?")
+        print("=" * 35)
+        print("[1] Aluno")
+        print("[2] Funcionário")
+        print("[3] Modalidade")
+        print("[4] Personal")
+        print("=" * 35)
+        type = int(input("Digite aqui: "))
+        if type == 1:
+            table = "alunos"
+            break
+        if type == 2:
+            table = "funcionarios"
+            break
+        if type == 3:
+            table = "modalidades"
+            break
+        if type == 4:
+            table = "personal"
+            break
+        else:
+            print("Digite um dígito válido.")
+    #Inserir
+    if choice == "1":
+        if table == "alunos":
+            nome = input("Digite o nome do aluno: ")
+            telefone = input(f"Digite o telefone do aluno {nome}: ")
+            endereco = input(f"Digite o endereço do aluno {nome}: ")
+            cpf = input(f"Digite o CPF do aluno {nome}: ")
+            email = input(f'Digite o email do aluno {nome}: ')
+            val = (nome, telefone, endereco, cpf, email)
+            sql = "INSERT INTO alunos (NOME, TELEFONE, ENDEREÇO, CPF, EMAIL) VALUES (%s, %s, %s, %s, %s)"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print(" ALUNO ADICIONADO COM SUCESSO!")
+        if table == "personal":
+            nome = input("Digite o nome do personal: ")
+            cpf = input(f"Digite o CPF do personal {nome}: ")
+            cref = input(f"Digite o CREF do personal {nome}: ")
+            telefone = input(f"Digite o telefone do personal {nome}: ")
+            endereco = input(f"Digite o endereço do personal {nome}: ")
+            val = (cpf, cref, nome, telefone, endereco)
+            sql = "INSERT INTO personal (CPF, CREF, NOME, TELEFONE, ENDEREÇO) VALUES (%s, %s, %s, %s, %s)"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("PERSONAL ADICIONADO COM SUCESSO!")
+        if table == "funcionarios":
+            nome = input("Digite o nome do funcionario: ")
+            cpf = input(f"Digite o CPF do funcionario {nome}: ")
+            salario = input(f"Digite o salario do funcionario {nome}: ")
+            val = (nome, cpf, salario)
+            sql = "INSERT INTO funcionarios (NOME, CPF, SALARIO) VALUES (%s, %s, %s)"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("FUNCIONÁRIO ADICIONADO COM SUCESSO!")
+        if table == "modalidades":
+            nome = input("Digite o nome da modalidade: ")
+            desc = input(f"Digite a descrição da modalidade {nome}:")
+            duracao = input(f"Digite a duração da modalidade {nome}: ")
+            val = (nome, desc, duracao)
+            sql = "INSERT INTO modalidades (NOME, DESCRICAO, DURACAO) VALUES (%s, %s, %s)"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("MODALIDADE ADICIONADA COM SUCESSO!")
+    #Remover
+    elif choice == "2":
+        if table == "alunos":
+            valor = input("Digite a matrícula do aluno que deseja remover: ")
+            val = (valor, )
+            sql = f"DELETE FROM {table} WHERE id = %s"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("ALUNO REMOVIDO COM SUCESSO!")
+        if table == "personal":
+            valor = input("Digite o CREF do personal que deseja remover: ")
+            val = (valor, )
+            sql = f"DELETE FROM {table} WHERE id = %s"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("PERSONAL REMOVIDO COM SUCESSO!")
+        if table == "funcionarios":
+            valor = input("Digite o ID do funcionário que deseja remover: ")
+            val = (valor, )
+            sql = f"DELETE FROM {table} WHERE id = %s"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("FUNCIONÁRIO REMOVIDO COM SUCESSO!")
+        if table == "modalidades":
+            valor = input("Digite o ID da modalidade que deseja remover: ")
+            val = (valor, )
+            sql = f"DELETE FROM {table} WHERE id = %s"
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print("=" * 35)
+            print("MODALIDADE REMOVIDA COM SUCESSO!")
+    #Procurar
+    elif choice == "3":
+        if table == "alunos":
+            mycursor.execute(f"SELECT * FROM {table}")
+            resultado = mycursor.fetchall()
+
+            for aluno in resultado:
+                print("=" * 35)
+                print(f"MATRÍCULA: {aluno[0]}")
+                print(f"NOME: {aluno[1]}")
+                print(f"TELEFONE: {aluno[2]}")
+                print(f"ENDEREÇO: {aluno[3]}")
+                print(f"CPF: {aluno[4]}")
+                print(f"EMAIL: {aluno[5]}")
+                print("=" * 35)
+        if table == "personal":
+            mycursor.execute(f"SELECT * FROM {table}")
+            resultado = mycursor.fetchall()
+
+            for personal in resultado:
+                print("=" * 35)
+                print(f"NOME: {personal[0]}")
+                print(f"CPF: {personal[1]}")
+                print(f"CREF: {personal[2]}")
+                print(f"TELEFONE: {personal[3]}")
+                print(f"ENDEREÇO: {personal[4]}")
+                print("=" * 35)
+        if table == "modalidades":
+            mycursor.execute(f"SELECT id, nome, descricao, duracao FROM {table}")
+            resultado = mycursor.fetchall()
+
+            for modalidade in resultado:
+                print("=" * 35)
+                print(f"ID: {modalidade[0]}")
+                print(f"NOME: {modalidade[1]}")
+                print(f"DESCRIÇÃO: {modalidade[2]}")
+                print(f"DURAÇÃO: {modalidade[3]}")
+                print("=" * 35)
+        if table == "funcionarios":
+            mycursor.execute(f"SELECT id, nome, cpf, salario FROM {table}")
+            resultado = mycursor.fetchall()
+
+            for funcionario in resultado:
+                print("=" * 35)
+                print(f"ID: {funcionario[0]}")
+                print(f"NOME: {funcionario[1]}")
+                print(f"CPF: {funcionario[2]}")
+                print(f"SALÁRIO: {funcionario[3]}")
+                print("=" * 35)
+    elif choice == "4":
+        while True:
+            doubt = input("DESEJA SAIR? DIGITE 1 PARA CONFIRMAR OU 2 PARA VOLTAR AO MENU.")
+            if doubt != "1" and doubt != "2":
+                ...
+            else:
+                break
+        if doubt == "1":
+            break
+        if doubt == "2":
+            ...
+print("Programa finalizado!")
